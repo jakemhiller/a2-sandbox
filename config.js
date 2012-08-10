b5d10f51713465c5cfe86c4c98a2d0bd9d7df3fa
Gabriel Farrell <g@grrawr.com>
2012-08-10 14:13:13 -0400

    Auto-loading of content type modules

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(app, express) {
-  app.use(express.logger('dev'));
-  app.use(express.static('static'));
-};
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 var util = require('util');
 
 var app = express();
 var port = process.env.PORT || 1168;
+var config = JSON.parse(fs.readFileSync('config.json'));
 
-// Load the settings
-require("./config.js")(app, express);
+app.contentTypes = config.contentTypes.map(function(ct) {
+  require(ct);
+});
+
+app.use(express.logger('dev'));
+app.use(express.static('static'));
 
 app.listen(port, function() {
   util.log('Listening on port ' + port);
