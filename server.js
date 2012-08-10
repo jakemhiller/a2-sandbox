bd16763eda394a35381b212cd73b58b61c7ef595
Gabriel Farrell <g@grrawr.com>
2012-08-10 10:57:56 -0400

    Added logging

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var util = require('util');
 var app = express();
 var port = process.env.PORT || 1168;
 
+app.use(express.logger('dev'));
 app.use(express.static('static'));
 
 app.listen(port, function() {
