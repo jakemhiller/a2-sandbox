0272704a3b24a1e7086adcf82d3f68e4d8277ae4
Gabriel Farrell <g@grrawr.com>
2012-08-10 10:41:34 -0400

    Added server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var util = require('util');
+
+var app = express();
+var port = process.env.PORT || 1168;
+
+app.use(express.static('static'));
+
+app.listen(port, function() {
+  util.log('Listening on port ' + port);
+});
