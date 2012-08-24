ccca9ed103f1dd88b65edef0137ec546a32f2cdc
Tom Boutell <tom@punkave.com>
2012-08-24 16:53:27 -0400

    Proof of concept for building script blocks in an Express 3.0 helper. That needs to migrate into a2-core

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var app = express();
 app.set('view engine', 'jade');
 var port = process.env.PORT || 1168;
 var config = JSON.parse(fs.readFileSync('config.json'));
+var a2 = require('a2-core');
 
+// a2.registerTypes(config.itemTypes);
 app.itemTypes = config.itemTypes.map(function(ct) {
   require(ct);
 });
@@ -16,12 +18,13 @@ app.use(express.logger('dev'));
 // If static can't find something other routes get a chance to find it.
 app.use(express.static('static'));
 
+app.locals.a2scripts =
 app.locals.a2scripts = function()
 {
   var data = ['/a2/core/js/core.js', '/a2/core/js/area.js', '/a2/core/js/itemWrapper.js', '/a2/itemTypes/text/js/type.js'];
   return _.reduce(data, function(memo, script) {
-    return memo + '<script src="' + encodeURIComponent(script) + '"></script>' + "\n";
-  });
+    return memo + '<script src="' + script + '"></script>' + "\n";
+  }, '');
 }
 
 app.get('/', function(req, res) {
