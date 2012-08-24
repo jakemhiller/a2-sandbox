2de6d4d29e0981e71f08fc34557d7009d2f8bdef
Tom Boutell <tom@punkave.com>
2012-08-24 15:43:52 -0400

    Serving it via a homepage route and jade

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var util = require('util');
 
 var app = express();
+app.set('view engine', 'jade');
 var port = process.env.PORT || 1168;
 var config = JSON.parse(fs.readFileSync('config.json'));
 
@@ -11,7 +12,13 @@ app.contentTypes = config.contentTypes.map(function(ct) {
 });
 
 app.use(express.logger('dev'));
+
+// If static can't find something other routes get a chance to find it.
 app.use(express.static('static'));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
 
 app.listen(port, function() {
   util.log('Listening on port ' + port);
