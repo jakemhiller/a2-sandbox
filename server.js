16a6c82330c7f02ac25c1f688f244c87bee73ba5
Tom Boutell <tom@punkave.com>
2012-08-24 16:45:34 -0400

    Committing for Jake

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var fs = require('fs');
 var util = require('util');
-
+var _ = require('underscore');
 var app = express();
 app.set('view engine', 'jade');
 var port = process.env.PORT || 1168;
 var config = JSON.parse(fs.readFileSync('config.json'));
 
-app.contentTypes = config.contentTypes.map(function(ct) {
+app.itemTypes = config.itemTypes.map(function(ct) {
   require(ct);
 });
 
@@ -15,6 +15,14 @@ app.use(express.logger('dev'));
 
 // If static can't find something other routes get a chance to find it.
 app.use(express.static('static'));
+
+app.locals.a2scripts = function()
+{
+  var data = ['/a2/core/js/core.js', '/a2/core/js/area.js', '/a2/core/js/itemWrapper.js', '/a2/itemTypes/text/js/type.js'];
+  return _.reduce(data, function(memo, script) {
+    return memo + '<script src="' + encodeURIComponent(script) + '"></script>' + "\n";
+  });
+}
 
 app.get('/', function(req, res) {
   res.render('index');
