c1a09a1f2dff9397ebeda15a274ea7c4d82289e5
Tom Boutell <tom@punkave.com>
2012-08-26 17:23:31 -0400

    mocha-powered unit tests showing that the openArea/validateArea dance works, including validation at the item type level

--- test/a2sandbox_test.js
+++ test/a2sandbox_test.js
@@ -1,5 +1,3 @@
-//var a2sandbox = require('../lib/a2sandbox.js');
-
 /*
   ======== A Handy Little Nodeunit Reference ========
   https://github.com/caolan/nodeunit
@@ -18,8 +16,77 @@
     test.throws(block, [error], [message])
     test.doesNotThrow(block, [error], [message])
     test.ifError(value)
+
+  To run them:
+
+  nodeunit a2sandbox_test.js
 */
 
+var _ = require('underscore');
+
+exports['suite'] = {
+  setUp: function(done) {
+    done();
+  },
+
+  'open': function(test) {
+    test.expect(10);
+    // Mock an express app object as a context
+    var app = { locals: {}, use: function() {} };
+    var a2 = require('a2-core');
+    var config = {
+      "defaultItemType": "a2-text",
+      "itemTypeNames": [
+        "a2-text",
+        "a2-files"
+      ]
+    };
+
+    config.itemTypes = config.itemTypeNames.map(function(itemTypeName) {
+      return require(itemTypeName);
+    });
+
+    a2.bootstrap(app, config);
+    test.ok(app.a2);
+    test.ok(app.locals.a2);
+
+    var data = {
+      itemInfos: [
+        {
+          type: 'a2-text',
+          data: {
+            'text': '<p>Once I met a man who had no <b>hat</b>.</p><p><a href="javascript:alert(5)">Forbidden</a><a href="http://permitted.example.com/">Permitted</a></p>'
+          }
+        },
+        {
+          type: 'a2-text',
+          data: {
+            text: 'I was sad because I had no fork.'
+          }
+        }
+      ]
+    };
+
+    app.a2.openArea(data, {}, function(err, data) {
+      test.ok(!err);
+      test.ok(data);
+
+      app.a2.validateArea(data, {}, function(err, data) {
+        test.ok(!err);
+        test.ok(data);
+        test.ok(data.itemInfos.length === 2);
+        _.each(data.itemInfos, function(itemInfo) {
+          test.ok(itemInfo.permId.length);
+        });
+        // The a2-text validator should have successfully blown away the XSS threat
+        test.ok(data.itemInfos[0].data.text.indexOf('href="javascript:') === -1);
+        // app.a2.log(data);
+        test.done();
+      });
+    });
+
+  }
+}
 // exports['awesome'] = {
 //   setUp: function(done) {
 //     // setup here
